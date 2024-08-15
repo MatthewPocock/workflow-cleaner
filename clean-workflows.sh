49d94219576bba5c1c9ffc9c5ee7fc90aa08ea61
mattpocock <matthew.pocock@outlook.com>
2024-08-15 21:47:51 +1000

    tidy output

--- clean-workflows.sh
+++ clean-workflows.sh
@@ -4,23 +4,23 @@ repo="MatthewPocock/workflow-cleaner"
 
 select_workflow() {
   gh api --paginate "/repos/$repo/actions/workflows" \
-    | jq -r '.workflows[] | [.id, .name, .path] | @tsv' \
+    | jq -r '.workflows[] | [.id, .name] | @tsv' \
     | fzf --multi
 }
 
 select_runs() {
-  while read -r id name; do
+  while IFS=$'\t' read -r id name; do
     gh api --paginate "/repos/$repo/actions/workflows/$id/runs" \
-      | jq -r '.workflow_runs[] | [.id, .name, .display_title] | @tsv'
+      | jq -r '.workflow_runs[] | [.id, .name] | @tsv'
   done
 }
 
 delete_run() {
-  while IFS=$'\t' read -r id name display_title; do
-    echo "Deleting run: $id - $name - $display_title"
-      gh api -X DELETE "/repos/$repo/actions/runs/$id" --silent \
-       && echo "Deleted ✅" \
-       || echo "Failed! ❌"
+  while IFS=$'\t' read -r id name; do
+    echo "Deleting run: $name - $id"
+    gh api -X DELETE "/repos/$repo/actions/runs/$id" \
+     && echo "Deleted ✅" \
+     || echo "Failed! ❌"
     sleep .1
   done
 }
